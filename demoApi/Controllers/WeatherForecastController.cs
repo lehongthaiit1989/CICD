bb80f1844f4211dc85a95faf0a6e4b37e899e962
ngoalongPro <thaitinmo@gmail.com>
2026-06-12 05:43:15 +0700

    dgấ

--- demoApi/Controllers/WeatherForecastController.cs
+++ demoApi/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace demoApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly1", "Cool1", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
